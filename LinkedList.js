22798eabe1170de47105c56a715a82497e36edd9
Richard Aguilar <ryaguilar23@gmail.com>
2018-06-14 19:42:31 -0400

    Update LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -13,32 +13,35 @@ LinkedList.prototype.attach = function(value) { //This creates a new method call
   var node = new Node(value); // A new node object is created
   currentNode = this.head; //This initialize the count (we start at the head of the list)
 
-  if (currentNode === null) {
+  if (currentNode === null) { //This checks to see if a node already exists. If it doesn't, it means the linkedlist is empty
     this.head = node;
     this.length++;
   } else {
-    while (currentNode.next) {
+    while (currentNode.next) { //This checks to see if the linked list has more than one node. If it does, it will keep iterating until it reaches the end of the list 
       currentNode = currentNode.next;
       this.length++;
     };
-    currentNode.next = node;
+    currentNode.next = node; //Once the end of the list is reached, a new link in the chain is created, and a node is attached.
     this.length++;
   };
 
 };
 
+//The code below creates a remove method that all linkedlists will inherit. It will keep track of the two positions in the node.
+//It will iterate through the linked list until it finds the node the user wants to remove. Once it does, it will set the node before
+//the targeted one equal to the node AFTER the targeted one. This will unlink the targeted node, effectively removing it.
 LinkedList.prototype.remove = function(removePosition) {
-  previousNode = null;
+  previousNode = null; 
   currentNode = this.head
   nodePosition = 1;
-  if (removePosition > s.length) {
+  if (removePosition > this.length) {
     console.log("The node you wish to delete does not exist!");
   } else {
-    while (nodePosition != removePosition) {
+    while (nodePosition != removePosition) { //This condition will be true until the targeted node is reached
       nodePosition++;
       previousNode = currentNode;
-      currentNode = currentNode.next;
+      currentNode = currentNode.next; 
     };
-    previousNode.next = currentNode.next;
+    previousNode.next = currentNode.next; //The node before the target will be linked to the node after the target, unlinking the targeted node
   };
 };
