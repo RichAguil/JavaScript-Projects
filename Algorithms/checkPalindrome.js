87a558ab46ad79682bd544a3892904daf73893b7
Richard Aguilar <ryaguilar23@gmail.com>
2019-09-13 00:49:54 -0400

    Delete checkPalindrome.js

--- Algorithms/checkPalindrome.js
+++
@@ -1,9 +0,0 @@
-function checkPalindrome(string) {
-  var stringArray = string.split('');
-  var reversedArray = stringArray.slice().reverse();
-  if (stringArray.join('')===reversedArray.join('')) {
-    return true;
-  } else {
-    return false;
-  };
-};
